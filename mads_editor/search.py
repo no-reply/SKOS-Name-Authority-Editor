9181b5db0d2540032eed3dce1906df60504505ce
Tom Johnson <thomas.johnson@oregonstate.edu>
2012-07-18 10:58:15 -0700

    improved search for regex matching

--- mads_editor/search.py
+++ mads_editor/search.py
@@ -1,7 +1,6 @@
-import django
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
 from django.conf import settings
+from django.forms import Form, CharField
+from django.shortcuts import render_to_response
 from SPARQLWrapper import SPARQLWrapper, JSON, N3
 from query import QueryManager
 from rdflib import URIRef
@@ -12,24 +11,42 @@ from rdflib import Literal
 
 def search(request, ref=None):
     search_form = SearchForm()
-    output = ''
+    hits = {}
+    rhits = {}
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            
             endpoint = SPARQLWrapper(settings.ENDPOINT)
             endpoint.setReturnFormat(JSON)
-            query = 'SELECT ?person ?name WHERE { ?person <http://xmlns.com/foaf/0.1/lastname> "%s" .} LIMIT 100' % form.cleaned_data['searchText']
+
+            query = """SELECT ?hit, ?regexhit WHERE 
+            {{?hit <http://xmlns.com/foaf/0.1/lastname> '%(s)s' .} UNION 
+             {?hit <http://xmlns.com/foaf/0.1/firstname> '%(s)s' .} UNION 
+             {?hit <http://xmlns.com/foaf/0.1/name> '%(s)s' .} UNION 
+             {?regexhit <http://www.w3.org/2004/02/skos/core#label> ?name FILTER regex(?name, '%(s)s', "i")} UNION 
+             {?regexhit <http://www.w3.org/2004/02/skos/core#altLabel> ?name FILTER regex(?name, '%(s)s', "i")} UNION 
+             {?hit <http://www.w3.org/2004/02/skos/core#label> '%(s)s' .} UNION
+             {?hit <http://www.w3.org/2004/02/skos/core#altLabel> '%(s)s' .}} 
+             LIMIT 100""" % {'s': form.cleaned_data['searchText']}
+
             endpoint.setQuery(query)
             r = endpoint.query().convert()
+            for person in r['results']['bindings']:
+                level = 'hit'
+                try:
+                    uri = person['hit']['value']
+                except:
+                    uri = person['regexhit']['value']
+                    level = 'regex'
+                suri = str(uri)
+                query = 'DESCRIBE <' + suri + '>'
+                endpoint.setQuery(query)
+                desc = endpoint.query().convert()
+                if level == 'hit':
+                    hits[suri] = desc[uri][u'http://www.w3.org/2004/02/skos/core#label'][0][u'value']
+                elif not suri in hits:
+                    rhits[suri] = desc[uri][u'http://www.w3.org/2004/02/skos/core#label'][0][u'value']
+    return render_to_response("search.tpl", {'form':search_form, 'hits': hits, 'rhits': rhits})
 
-            output = output + '<table>\n'
-            output = output + str(r['results'])
-      #      for person in r['results']['bindings']:
-      #          output = output + "<tr>\n<td>" + person['person']['value'] + '</td><td>' + person['name']['value'] + "</td>\n</tr>\n"
-
-            output = output + '</table>'
-    return render_to_response("search.tpl", {'form':search_form, 'table': output})
-
-class SearchForm(django.forms.Form):
-    searchText = django.forms.CharField()
+class SearchForm(Form):
+    searchText = CharField()
--- tpl/resource.tpl
+++ tpl/resource.tpl
@@ -2,26 +2,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="{{lang}}" lang="{{lang}}">
   <head>
-    <title>MADS Search</title>
+    <title>{{ short }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   </head>
   <body>
-    <form action="/" method="post"> {% csrf_token %}
-      {{form.as_p}}
-    </form>
+    <h2><a href="{{ uri }}">{{ short }}</a></h2>
     <table>
-      {% for hit, desc in hits.items %}
+      {% for field, value in res.items %}
         <tr>
-	  <td>{{ hit }}</td>
-	  <td>{{ desc }}</td>
-	</tr>
-      {% endfor %}
-    </table>
-    <table>
-      {% for hit, desc in rhits.items %}
-        <tr>
-	  <td>{{ hit }}</td>
-	  <td>{{ desc }}</td>
+	  <td>{{ field }}</td>
+	  <td>{{ value.0.value }}</td>
 	</tr>
       {% endfor %}
     </table>
